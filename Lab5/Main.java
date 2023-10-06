80bb8560214846d919c299d49506ee054ab598ef
John Loveland <john.loveland@S6445518outlook.onmicrosoft.com>
2023-10-05 20:52:59 -0500

    Added Nodes and Connections to project.

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -1,20 +1,23 @@
 package Lab5;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         FileReader reader = new FileReader("squares.txt");
-        Sqr[][] squares = reader.readFile(new Sqr[6][7]);
+        ArrayList<ShippingNode> nodes = reader.readFile();
 
-        var start = squares[0][0];
+        // Todo - Flesh out this code, it will come from the file and need to be passed
+        // out of the readfile class.
+        // We will get to that. For now, will manually add the start and end nodes when
+        // testing code.
+        ShippingNode start = new ShippingNode("someNode");
         start.isStart = true;
-        var end = squares[3][6];
+        ShippingNode end = new ShippingNode("someOtherNode");
         end.isEnd = true;
 
-        PathFinder pathFinder = new PathFinder(squares);
-        pathFinder.setNeighbors();
-        pathFinder.drawSquares();
-        System.out.println(pathFinder.findPath(start, end));
+        PathFinder.findPath(start, end);
+
     }
 }
--- Lab5/PathFinder.java
+++ Lab5/PathFinder.java
@@ -4,49 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PathFinder {
-	// main list of squares, owned by aggregation in path finder class
-	private Sqr[][] squares;
-
-	// constructor of Pathfinder class. Potentially will need a deep copy here
-	// depending on behavior of main method. It's important that the vehicles
-	// stay in the ending position after each peice of cargo moves through the
-	// network
-	public PathFinder(Sqr[][] squares) {
-		this.squares = squares;
-	}
-
-	public ArrayList<Sqr> findPath(Sqr start, Sqr end) {
-		// path tracks the best path as each square is tested
-		ArrayList<Sqr> path = null;
+	public static ArrayList<ShippingNode> findPath(ShippingNode start, ShippingNode end) {
+		ArrayList<ShippingNode> path = null;
 		// open tracks the squares that haven't been closed yet.
-		ArrayList<Sqr> open = new ArrayList<Sqr>();
+		ArrayList<ShippingNode> open = new ArrayList<ShippingNode>();
 		// close tracks the squares we have already examined.
-		ArrayList<Sqr> closed = new ArrayList<Sqr>();
+		ArrayList<ShippingNode> closed = new ArrayList<ShippingNode>();
 		// add the first node to the open list prior to beginning
 		open.add(start);
 		// initialize the path to get to for the initial square, as this square has
 		// never been visited before.
-		start.pathToGetTo = new ArrayList<Sqr>(Arrays.asList(start));
+		start.pathToGetTo = new ArrayList<ShippingNode>(Arrays.asList(start));
 		// loop while the open list has squares on it.
 		while (!open.isEmpty()) {
 			// Get the first item from open list and set as current
-			Sqr current = open.get(0);
+			ShippingNode current = open.get(0);
 			// Remove the first item from the open list (we will add it to closed at the
 			// end)
 			open.remove(0);
-			// output the current x and y value of the square we are looking at to the
-			// console
-			System.out.println("Current: " + current.y + ", " + current.x);
 			// Check if we are at the end
 			if (current.isEnd) {
 				// It we are at the end, output the path cost
-				System.out.println("Found path: " + Sqr.getPathToCost(current.pathToGetTo));
+				System.out.println("Found path: " + ShippingNode.getPathToCost(current.pathToGetTo));
 				// If we are at the end set the path variable if it hasn't been set
 				if (path == null)
 					path = current.pathToGetTo;
 				// Error handling, we check the current path if it already exists and compare it
 				// to the path to get to of the current square, then adjust the path
-				else if (Sqr.getPathToCost(path) > Sqr.getPathToCost(current.pathToGetTo))
+				else if (ShippingNode.getPathToCost(path) > ShippingNode.getPathToCost(current.pathToGetTo))
 					path = current.pathToGetTo;
 			}
 			// if we aren't at the end place the current square on the closed list.
@@ -55,61 +40,38 @@ public class PathFinder {
 			}
 
 			// get all the adjacent squares and iterate over them.
-			ArrayList<Sqr> adj = current.neighbors;
-			for (Sqr s : adj) {
+			ArrayList<ShippingNodeConnection> neighborConnections = current.neighbors;
+			for (ShippingNodeConnection connection : neighborConnections) {
 				// if the suqare isn't on the closed list AND hasn't been added to the open list
-				if (!closed.contains(s) && !open.contains(s)) {
+				ShippingNode neighborNode = connection.getNodeConnection(current);
+				if (!closed.contains(neighborNode) && !open.contains(neighborNode)) {
 					// deep copy the path to get to of the current square
-					ArrayList<Sqr> copy = new ArrayList<Sqr>(current.pathToGetTo);
+					ArrayList<ShippingNode> pathtoCurrent = new ArrayList<>(current.pathToGetTo);
 					// add"s", the adjecent square to the list
-					copy.add(s);
+					pathtoCurrent.add(neighborNode);
 					// set s's path to copy (so it knows how to reach itself)
-					s.pathToGetTo = copy;
+					neighborNode.pathToGetTo = pathtoCurrent;
+
 					// add the modified adjacent square to the open list
-					open.add(s);
+					open.add(neighborNode);
 				} else {
-					// if the square is on the closed list, deep copy the path to get to of the
+					// if the square is on the closed list, deep copy the path to get the
 					// current square
-					ArrayList<Sqr> copy = new ArrayList<Sqr>(current.pathToGetTo);
+					ArrayList<ShippingNode> pathtoCurrent = new ArrayList<ShippingNode>(current.pathToGetTo);
 					// add"s", the adjecent square to the list
-					copy.add(s);
+					pathtoCurrent.add(neighborNode);
 					// check if you have found a better path to s
-					if (Sqr.getPathToCost(s.pathToGetTo) > Sqr.getPathToCost(copy)) {
-						s.pathToGetTo = copy;
+					if (ShippingNode.getPathToCost(neighborNode.pathToGetTo) > ShippingNode
+							.getPathToCost(pathtoCurrent)) {
+						neighborNode.pathToGetTo = pathtoCurrent;
 						// remove s from closed list and re-add it to open list, since we now have a
 						// better path and we need to test it
-						closed.remove(s);
-						open.add(s);
+						closed.remove(neighborNode);
+						open.add(neighborNode);
 					}
 				}
 			}
 		}
 		return path;
 	}
-
-	public void setNeighbors() {
-		// This method iterates over all of the squares and calls the square's
-		// setNeighbors method,
-		// the square finds it's immediate neighbors and stores them in it's own list so
-		// it know who it's neighbors are
-		for (int r = 0; r < squares.length; r++)
-			for (int c = 0; c < squares[r].length; c++)
-				squares[r][c].setNeighbors(squares);
-	}
-
-	public void drawSquares() {
-		// iterate over all the squares and print them to the console so you
-		// can see the network visually on the screen.
-		for (int r = 0; r < squares.length; r++) {
-			// top row for squares
-			for (int c = 0; c < squares[r].length; c++)
-				squares[r][c].drawTop();
-			System.out.println();
-			// bottom row
-			for (int c = 0; c < squares[r].length; c++)
-				squares[r][c].drawBottom();
-			System.out.println();
-		}
-	}
-
 }
